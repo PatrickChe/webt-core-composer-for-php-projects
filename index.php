eff2a0840d9894e28d7bb3424269ef2d677fbde0
flo1a <105285535+flo1a@users.noreply.github.com>
2024-02-14 13:01:28 +0100

    1

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+require_once "../webt-core-composer-for-php-projects/vendor/autoload.php";
+use Endroid\QrCode\Color\Color;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Label\Label;
+use Endroid\QrCode\Logo\Logo;
+use Endroid\QrCode\RoundBlockSizeMode;
+use Endroid\QrCode\Writer\PngWriter;
+use Endroid\QrCode\Writer\ValidationException;
+
+$writer = new PngWriter();
+
+// Create QR code
+$qrCode = QrCode::create('+43 1 22 33 444')
+    ->setEncoding(new Encoding('UTF-8'))
+    ->setErrorCorrectionLevel(ErrorCorrectionLevel::Low)
+    ->setSize(300)
+    ->setMargin(10)
+    ->setRoundBlockSizeMode(RoundBlockSizeMode::Margin)
+    ->setForegroundColor(new Color(0, 0, 0))
+    ->setBackgroundColor(new Color(255, 255, 255));
+
+$result = $writer->write($qrCode);
+
+header('Content-Type: ' . $result->getMimeType());
+echo $result->getString();
+?>
